29125ea7944ded38490dbedc7f4bdcb5c2ab3bac
Soburr <adebesinbenzenejr99@gmail.com>
2021-12-28 22:10:28 +0100

    authentication from login to crud

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,17 +20,13 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            {{-- <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1>Hooray!!!! YOU ARE LOGGED IN</h1>
-                </div>
-            </header> --}}
+         
+            <main>
+                @yield('content')
+            </main>
 
-            <!-- Page Content -->
-            {{-- <main>
-                {{ $slot }}
-            </main> --}}
+            
+
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-}); 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-
-Route::resource('student', 'App\Http\Controllers\StudentsController');
+Route::view('/', 'welcome'); 
+Route::group(['middleware' => 'auth'], function () {
+    // Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::resource('student', 'App\Http\Controllers\StudentsController');  
+});
 
 
 
